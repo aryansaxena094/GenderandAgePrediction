dcda7c3d3a071aa051e6f65d0a335e9f64451351
Aryan Saxena <aryan.saxena094@gmai.com>
2021-01-15 17:35:32 +0530

    ENIT PUSH

---
+++ face.py
@@ -0,0 +1,49 @@
+import cv2
+import math
+import argparse
+
+def facehighlight(net, frame, conf_threshold=0.7):
+    frameOpencvDnn=frame.copy()
+    frameHeight=frameOpencvDnn.shape[0]
+    frameWidth=frameOpencvDnn.shape[1]
+    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections=net.forward()
+    faceBoxes=[]
+    for i in range(detections.shape[2]):
+        confidence=detections[0,0,i,2]
+        if confidence>conf_threshold:
+            x1=int(detections[0,0,i,3]*frameWidth)
+            y1=int(detections[0,0,i,4]*frameHeight)
+            x2=int(detections[0,0,i,5]*frameWidth)
+            y2=int(detections[0,0,i,6]*frameHeight)
+            faceBoxes.append([x1,y1,x2,y2])
+            cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,0,255), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn,faceBoxes
+
+parser=argparse.ArgumentParser()
+parser.add_argument('--image')
+
+args=parser.parse_args()
+
+faceProto="opencv_face_detector.pbtxt"
+faceModel="opencv_face_detector_uint8.pb"
+
+faceNet=cv2.dnn.readNet(faceModel,faceProto)
+
+video=cv2.VideoCapture(args.image if args.image else 0)
+padding=20
+while cv2.waitKey(1)<0:
+    hasFrame,frame=video.read()
+    if not hasFrame:
+        cv2.waitKey()
+        break
+
+    resultImg,faceBoxes=facehighlight(faceNet,frame)
+    if not faceBoxes:
+        print("No face detected")
+    
+    
+
+    cv2.imshow("Detecting face", resultImg)
